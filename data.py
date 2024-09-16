41a9b61ba9259f900f037358f00406fe288b1eda
ca5500 <cassidyw11@nycstudents.net>
2024-09-16 09:20:15 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,27 @@
+x = 3
+y = float(3)
+print(x,y)
+
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+    x = "test"
+print(f"hello {x}")
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+
+x = input("pick a number")
+number = 8
+if number is (2,4,6,8,10):
+    print("even")
+if number is (1,3,5,7,9):
+    print ("odd")
+
